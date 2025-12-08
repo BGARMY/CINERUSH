1083f650ac1456d3b49f25989eb9362d4d48b6e3
Mattaparthi Sivannarayana <mattaparthisivannarayana@gmail.com>
2025-12-08 13:26:34 +0530

    Initial commit

---
+++ backend/routes/ticket_routes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const { getTicketByBookingId } = require("../controllers/ticket_controller");
+
+router.get("/:id", getTicketByBookingId);
+
+module.exports = router;
